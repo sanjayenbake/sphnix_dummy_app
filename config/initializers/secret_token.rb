c3b4b0ec95dfb2cb2849fd61b0c42c84a848346c
tpsingh <tpsingh@enbake.com>
2014-08-08 12:25:15 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SphnixDummyApp::Application.config.secret_key_base = '2ba22c61f48ddf9b1436eecf64a65096f152786c760039d790d7af43df0f3085ee37150bc752f31b432f1fc69f60de398f841fd9d4e19c9f389f84ff7f1a6a4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SphnixDummyApp::Application.config.session_store :cookie_store, key: '_sphnix_dummy_app_session'
